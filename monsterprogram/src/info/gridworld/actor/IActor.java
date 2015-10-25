34d3828aec37e0f3c8949574add81c42e72e28db
turtlemurphy <turtlemurphy@gmail.com>
2015-10-24 20:49:39 -0500

    Update 7

--- monsterprogram/src/info/gridworld/actor/IActor.java
+++ monsterprogram/src/info/gridworld/actor/IActor.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package info.gridworld.actor;
 
-/**
- *
- * @author Taylor
- */
-public interface IActor {
-    
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+import java.awt.Color;
+
+public interface IActor 
+{
+    Color getColor();
+    void setColor(Color newColor);
+    int getDirection();
+    void setDirection(int newDirection);
+    Grid<Actor> getGrid();
+    Location getLocation();
+    void putSelfInGrid(Grid<Actor> gr, Location loc);
+    void removeSelfFromGrid();
+    void moveTo(Location newLocation);
+    void act();
+    String toString();
 }
